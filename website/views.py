673453114934bfb63bdd8e92b5e6acffff4a4788
Moooogz <markdavedizon@yahoo.com>
2023-09-01 22:00:59 +0800

    modal added

--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Patient
 
 def home(request):
     patientsdata = Patient.objects.all()
    
     return render(request, 'home.html',{'patientsdata': patientsdata})
+
+
+def patientDetails(request):
+    return render(request, 'patientdetails.html',{})
